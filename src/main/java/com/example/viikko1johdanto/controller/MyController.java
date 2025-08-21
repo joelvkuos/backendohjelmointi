bcef94297bb862c60d8c0b24ae19bb12c105adc6
bhr469@myy.haaga-helia.fi <bhr469@myy.haaga-helia.fi>
2025-08-21 21:44:25 +0300

    Valmiit tehtävät

--- src/main/java/com/example/viikko1johdanto/controller/MyController.java
+++ src/main/java/com/example/viikko1johdanto/controller/MyController.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 @Controller
 
@@ -15,6 +16,20 @@ public class MyController {
             @RequestParam(name = "nimesi", required = false, defaultValue = "John") String etunimi,
             @RequestParam(name = "location", required = false, defaultValue = "Moon") String location) {
         return "Welcome to the " + location + " " + etunimi + "!";
+    }
+
+    @RequestMapping("index")
+    @ResponseBody
+    public String index() {
+        return "This is the main page";
+
+    }
+
+    @RequestMapping("contact")
+    @ResponseBody
+    public String contact() {
+        return "This is the contact page";
+
     }
 
 }
